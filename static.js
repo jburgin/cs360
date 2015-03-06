3024cced73c458fa09f32bd36905f3072a9566fa
jpburgin <jeffburgin@gmail.com>
2015-03-05 19:32:29 -0700

    working on lab

--- static.js
+++ static.js
@@ -2,6 +2,7 @@ var fs = require('fs');
 var http = require('http');
 var url = require('url');
 var ROOT_DIR = "html/";
+
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
   fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
@@ -10,17 +11,27 @@ http.createServer(function (req, res) {
       res.end(JSON.stringify(err));
       return;
     }
-    res.writeHead(200);
-    res.end(data);
+	
+	if (urlObj.pathname.indexOf("getcity") != -1) {
+		console.log("URL query " + urlObj.query["q"]);
+		fs.readFile('cities.dat.txt', function (err, data) {
+			if (err) throw err;
+			cities = data.toString().split("\n");
+			var myRe = new RegExp("^" + urlObj.query["q"]);
+			var jsonresult = [];
+			for (var i = 0; i < cities.length; i++) {
+				var result = cities[i].search(myRe);
+				if (result != -1) {
+					jsonresult.push({city:cities[i]});
+				}
+			}
+			res.writeHead(200);
+			res.end(JSON.stringify(jsonresult));
+		});
+	} else {
+		res.writeHead(200);
+		res.end(data);
+	}
+	
   });
 }).listen(80);
-
-function handleResponse(response) {
-  var serverData = '';
-  response.on('data', function (chunk) {
-    serverData += chunk;
-  });
-  response.on('end', function () {
-    console.log(serverData);
-  });
-}
