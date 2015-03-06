a6e62d465ac24c9638e0b006b86c9d68bbfc9687
jpburgin <jeffburgin@gmail.com>
2015-03-05 19:59:05 -0700

    making the city query case insensitive

--- static.js
+++ static.js
@@ -6,14 +6,14 @@ var ROOT_DIR = "html/";
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
 	if (urlObj.pathname.indexOf("getcity") != -1) {
-		console.log("URL query " + urlObj.query["q"]);
 		fs.readFile('cities.dat.txt', function (err, data) {
 			if (err) throw err;
 			var cities = data.toString().split("\n");
-			var myRe = new RegExp("^" + urlObj.query["q"]);
+			var myRe = new RegExp("^" + urlObj.query["q"] + "i");
+			console.log(myRe);
 			var jsonresult = [];
 			for (var i = 0; i < cities.length; i++) {
-				var result = cities[i].search(myRe + "i");
+				var result = cities[i].search(myRe);
 				if (result != -1) {
 					jsonresult.push({city:cities[i]});
 				}
