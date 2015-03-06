779024f4132c708484876cfd28c4bca5898f655e
jpburgin <jeffburgin@gmail.com>
2015-03-05 19:44:32 -0700

    working on node server

--- static.js
+++ static.js
@@ -6,12 +6,12 @@ var ROOT_DIR = "html/";
 http.createServer(function (req, res) {
   var urlObj = url.parse(req.url, true, false);
   fs.readFile(ROOT_DIR + urlObj.pathname, function (err,data) {
+	console.log("URL query: " + urlObj.pathname);
     if (err) {
       res.writeHead(404);
       res.end(JSON.stringify(err));
       return;
     }
-	console.log("URL query: " + urlObj.pathname);
 	if (urlObj.pathname.indexOf("getcity") != -1) {
 		console.log("URL query " + urlObj.query["q"]);
 		fs.readFile('cities.dat.txt', function (err, data) {
