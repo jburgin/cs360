a1f14fb1bd5b26b635a8428905bee5ed1f7bc202
jpburgin <jeffburgin@gmail.com>
2015-03-26 19:20:32 -0600

    actually using the factory :)

--- lab9/static/lab9-app.js
+++ lab9/static/lab9-app.js
@@ -1,21 +1,16 @@
 angular.module('weatherNews', [])
 .factory('postFactory', [function() {
 	var o = {
-		posts: []
+		posts: [{title:'test', upvotes:15}]
 	};
 	return o;
 }])
 .controller('MainCtrl', [
   '$scope',
-  function($scope){
+  'postFactory',
+  function($scope, postFactory){
     $scope.test = 'Hello world!';
-    $scope.posts = [
-      {title:'Post 1', upvotes:5},
-      {title:'Post 2', upvotes:6},
-      {title:'Post 3', upvotes:1},
-      {title:'Post 4', upvotes:4},
-      {title:'Post 5', upvotes:3}
-    ];
+    $scope.posts = postFactory.posts;
 	$scope.addPost = function() {
 		$scope.posts.push({title:$scope.formContent, upvotes:0});
 		$scope.formContent='';
