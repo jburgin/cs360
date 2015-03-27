c0796c1a5faad868441668442dde7148fcd30a1e
jpburgin <jeffburgin@gmail.com>
2015-03-26 22:44:43 -0600

    added comment upvote route

--- lab9/weather-news/routes/index.js
+++ lab9/weather-news/routes/index.js
@@ -71,4 +71,11 @@ router.post('/posts/:post/comments', function(req, res, next) {
 	});
 });	
 
+router.post('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+	req.comment.upvote(function(err,comment) {
+		if (err) {return next(err);}
+		res.json(comment);
+	});
+});
+
 module.exports = router;
