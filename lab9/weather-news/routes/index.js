52aec494cda3f6feb3e04dd160070487f54544ef
jpburgin <jeffburgin@gmail.com>
2015-03-26 22:51:27 -0600

    put instead of post.....

--- lab9/weather-news/routes/index.js
+++ lab9/weather-news/routes/index.js
@@ -71,7 +71,7 @@ router.post('/posts/:post/comments', function(req, res, next) {
 	});
 });	
 
-router.post('/posts/:post/comments/:comment/upvote', function(req, res, next) {
+router.put('/posts/:post/comments/:comment/upvote', function(req, res, next) {
 	req.comment.upvote(function(err,comment) {
 		if (err) {return next(err);}
 		res.json(comment);
