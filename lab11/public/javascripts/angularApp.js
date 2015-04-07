4b05db185f986713337cb90758d26fba2cbec0f2
Jeff <jeffburgin@gmail.com>
2015-04-07 14:16:28 -0600

    loop

--- lab11/public/javascripts/angularApp.js
+++ lab11/public/javascripts/angularApp.js
@@ -2,16 +2,18 @@ angular.module('clusterApp', [])
 .controller('MainCtrl', [
   '$scope', '$http',
   function($scope, $http){
-    $scope.cluster = [{pid:1234}];
+    $scope.cluster = [];
 	$scope.getPIDs = function() {
 		$scope.cluster = [{pid:12},{pid:34}];
 	}
 	$scope.getMyPIDs = function() {
-		$http.get('/pid').success(function(data){
-		  console.log("getAll");
-		  console.log(data);
-		  $scope.cluster.push(data);
-		});
+		for (i = 0; i < 100; i++) {
+			$http.get('/pid').success(function(data){
+			  console.log("getAll");
+			  console.log(data);
+			  $scope.cluster.push(data);
+			});
+		}
     }
   } 
 ]);
