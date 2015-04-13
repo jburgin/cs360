0199d89a8c17e096f7c20815e7d96a4f535ea008
Jeff <jeffburgin@gmail.com>
2015-04-13 15:43:05 -0600

    routing stuff

--- lab10/cart_server.js
+++ lab10/cart_server.js
@@ -50,7 +50,6 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-require('./cart_routes')(app);
 app.use(cookieParser());
 app.use(expressSession({ secret: 'SECRET' }));
 app.use(passport.initialize()); 
@@ -84,11 +83,25 @@ app.get('/logout', function(req, res){
   req.logout();
   res.redirect('/login');
 });
-/*app.get('/info', function(req, res){
-  if(req.isAuthenticated()){
-    res.render('info', { user: req.user });
-  } else {
-    res.redirect('/login');
-  }
-});*/
+  var customers = require('./controllers/customers_controller');
+  var products = require('./controllers/products_controller');
+  var orders = require('./controllers/orders_controller');
+  app.use('/static', express.static( './static')).
+      use('/images', express.static( './images')).
+      use('/lib', express.static( './lib')
+  );
+  app.get('/', function(req, res){
+	if (req.user) {
+		res.render('shopping');
+	} else {
+		res.render('login', { user: req.user });
+	}
+  });
+  app.get('/products/get', products.getProducts);
+  app.get('/orders/get', orders.getOrders);
+  app.post('/orders/add', orders.addOrder);
+  app.get('/customers/get', customers.getCustomer);
+  app.post('/customers/update/shipping', customers.updateShipping);
+  app.post('/customers/update/billing', customers.updateBilling);
+  app.post('/customers/update/cart', customers.updateCart);
 app.listen(80);
