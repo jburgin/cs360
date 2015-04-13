5cbb090b05bb195259b61869fea6b1a71660dd36
Jeff <jeffburgin@gmail.com>
2015-04-13 15:48:24 -0600

    console logs

--- lab10/cart_server.js
+++ lab10/cart_server.js
@@ -92,6 +92,7 @@ app.get('/logout', function(req, res){
   );
   app.get('/', function(req, res){
 	if (req.user) {
+		console.log(user);
 		res.render('shopping', { user: req.user });
 	} else {
 		res.render('login', { user: req.user });
